b630323bf9bde17dabc81e785dce54fcaf144ab4
Takuya Hirata <takuyahirata4@gmail.com>
2023-03-26 10:49:02 -0400

    Config: docker, gitignore, database port

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,8 @@ config :trackers, Trackers.Repo,
   database: "trackers_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 10,
+  port: 2020
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
--- config/test.exs
+++ config/test.exs
@@ -11,7 +11,8 @@ config :trackers, Trackers.Repo,
   hostname: "localhost",
   database: "trackers_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+  pool_size: 10,
+  port: 2020
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
