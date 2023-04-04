44e73f15209d7355a0e62390b8c7473f28c3942f
Takuya Hirata <takuyahirata4@gmail.com>
2023-04-04 08:27:59 -0400

    Test: update auth tests

--- test/trackers_web/live/user_registration_live_test.exs
+++ test/trackers_web/live/user_registration_live_test.exs
@@ -5,23 +5,6 @@ defmodule TrackersWeb.UserRegistrationLiveTest do
   import Trackers.AccountsFixtures
 
   describe "Registration page" do
-    test "renders registration page", %{conn: conn} do
-      {:ok, _lv, html} = live(conn, ~p"/users/register")
-
-      assert html =~ "Register"
-      assert html =~ "Log in"
-    end
-
-    test "redirects if already logged in", %{conn: conn} do
-      result =
-        conn
-        |> log_in_user(user_fixture())
-        |> live(~p"/users/register")
-        |> follow_redirect(conn, "/")
-
-      assert {:ok, _conn} = result
-    end
-
     test "renders errors for invalid data", %{conn: conn} do
       {:ok, lv, _html} = live(conn, ~p"/users/register")
 
@@ -30,7 +13,6 @@ defmodule TrackersWeb.UserRegistrationLiveTest do
         |> element("#registration_form")
         |> render_change(user: %{"email" => "with spaces", "password" => "short"})
 
-      assert result =~ "Register"
       assert result =~ "must have the @ sign and no spaces"
       assert result =~ "should be at least 8 character"
     end
@@ -50,8 +32,6 @@ defmodule TrackersWeb.UserRegistrationLiveTest do
       # Now do a logged in request and assert on the menu
       conn = get(conn, "/")
       response = html_response(conn, 200)
-      assert response =~ email
-      assert response =~ "Settings"
       assert response =~ "Log out"
     end
 
@@ -68,20 +48,6 @@ defmodule TrackersWeb.UserRegistrationLiveTest do
         |> render_submit()
 
       assert result =~ "has already been taken"
-    end
-  end
-
-  describe "registration navigation" do
-    test "redirects to login page when the Log in button is clicked", %{conn: conn} do
-      {:ok, lv, _html} = live(conn, ~p"/users/register")
-
-      {:ok, _login_live, login_html} =
-        lv
-        |> element(~s|main a:fl-contains("Sign in")|)
-        |> render_click()
-        |> follow_redirect(conn, ~p"/users/log_in")
-
-      assert login_html =~ "Log in"
     end
   end
 end
