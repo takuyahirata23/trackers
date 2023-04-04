cd6060b28c60536feae624bede6a44792ae553ed
Takuya Hirata <takuyahirata4@gmail.com>
2023-04-04 08:27:59 -0400

    Test: update auth tests

--- test/trackers_web/controllers/page_controller_test.exs
+++ test/trackers_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TrackersWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Hello Track Addicts!"
   end
 end
--- test/trackers_web/live/user_login_live_test.exs
+++ test/trackers_web/live/user_login_live_test.exs
@@ -5,14 +5,6 @@ defmodule TrackersWeb.UserLoginLiveTest do
   import Trackers.AccountsFixtures
 
   describe "Log in page" do
-    test "renders log in page", %{conn: conn} do
-      {:ok, _lv, html} = live(conn, ~p"/users/log_in")
-
-      assert html =~ "Log in"
-      assert html =~ "Register"
-      assert html =~ "Forgot your password?"
-    end
-
     test "redirects if already logged in", %{conn: conn} do
       result =
         conn
@@ -58,18 +50,6 @@ defmodule TrackersWeb.UserLoginLiveTest do
   end
 
   describe "login navigation" do
-    test "redirects to registration page when the Register button is clicked", %{conn: conn} do
-      {:ok, lv, _html} = live(conn, ~p"/users/log_in")
-
-      {:ok, _login_live, login_html} =
-        lv
-        |> element(~s|main a:fl-contains("Sign up")|)
-        |> render_click()
-        |> follow_redirect(conn, ~p"/users/register")
-
-      assert login_html =~ "Register"
-    end
-
     test "redirects to forgot password page when the Forgot Password button is clicked", %{
       conn: conn
     } do
