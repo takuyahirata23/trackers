d992b4a6fd9b806ce932ecbe22bb518df03a2b23
Takuya Hirata <takuyahirata4@gmail.com>
2023-06-07 21:38:09 -0400

    Feat: show best laps per layout

--- lib/trackers/helpers/time.ex
+++ lib/trackers/helpers/time.ex
@@ -10,11 +10,19 @@ defmodule Trackers.Helpers.Time do
   def convert_milliseconds_to_lap_time(lap_time_in_milliseconds) do
     minute = Integer.floor_div(lap_time_in_milliseconds, 60000)
 
-    seconds =
+    [seconds, milliseconds] =
       (Integer.mod(lap_time_in_milliseconds, 60000) / 1000)
       |> Float.to_string()
-      |> String.replace(".", ":")
+      |> String.split(".")
+      |> Enum.map(&add_zero(&1))
 
-    "#{minute}:#{seconds}"
+    "#{minute}:#{seconds}:#{milliseconds}"
+  end
+
+  defp add_zero(time) do
+    case String.length(time) do
+      1 -> "0#{time}"
+      _ -> time
+    end
   end
 end
